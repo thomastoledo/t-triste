f3922185b0238ccd7c200a538ab0a0cad91f8ed4
ImFlog <garcia.florian.perso@gmail.com>
2021-02-18 22:40:21 +0100

    Stream 1: Le futur du battle royale est en marche (ou pas) :bomb:

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # t-triste
+
+Un puzzle game compétitif (`BATTLE ROYALE?` NON).
+
+## Stream 1
+### Réalisation
+* Présentation du projet
+* On a testé [Bevy](https://github.com/bevyengine/bevy)
+    * hello world
+    * prise en main des components
+    * prise en main des systems
+    * prise en main des plugins
+    * prise en main de nos vies
+    * prise en main du saumon
+
+## Stream 2
+### Interstream
+* Créer un asset de pièce de jeu
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+
+// le system
+fn add_people(commands: &mut Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if the timer hasn't finished yet, we return
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+// Components
+struct Person;
+
+struct Name(String);
+
+// Plugins
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
+
+// Start function
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
